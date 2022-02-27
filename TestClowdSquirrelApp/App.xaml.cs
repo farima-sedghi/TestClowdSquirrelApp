6058b31dd77c6a3bafa130a6606c74c1966264f2
farima sedghi <farima.ss2@gmail.com>
2022-02-27 15:31:39 +0330

    Add project files.

---
+++ TestClowdSquirrelApp/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestClowdSquirrelApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            try
+            {
+                SquirrelAwareApp.HandleEvents(
+             onInitialInstall: OnAppInstall,
+             onAppUninstall: OnAppUninstall,
+             onEveryRun: OnAppRun);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private static void OnAppInstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.CreateShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppUninstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.RemoveShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppRun(SemanticVersion version, IAppTools tools, bool firstRun)
+        {
+            tools.SetProcessAppUserModelId();
+            // show a welcome message when the app is first installed
+            //if (firstRun) MessageBox.Show("Thanks for installing my application!");
+        }
+    }
+}
